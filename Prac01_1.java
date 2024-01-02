693883699a4013b6c0733f17ba717d7b6a477fe1
Atman Mehta <130650083+Atman-Mehta@users.noreply.github.com>
2024-01-02 16:30:58 +0530

    Add files via upload

---
+++ Prac01_1.java
@@ -0,0 +1,20 @@
+//  Write a java program to find sum of two numbers using class.
+
+package JavaPracticals;
+
+import java.util.Scanner;
+
+public class Prac01_1 {
+	
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number 1: ");
+		a=sc.nextInt();
+		System.out.println("Enter number 2: ");
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(a+"+"+b+"="+c);
+	}
+
+}
